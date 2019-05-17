f31cadb9c52081c2aaeb36905c68f8e45f26da0a
Satyam Shukla <sam@pop-os.localdomain>
2019-05-17 07:20:10 +0530

    complete book list in profile

--- controllers/listcontroller.js
+++ controllers/listcontroller.js
@@ -3,10 +3,16 @@ var models = require('../models');
 exports.listbooks = function(req,res){
 	console.log(req.body);
 
-    models.owned.findAll({ where : {userId: req.user.id} }).then(books => {
+    models.book.findAll({ include:[{
+    	model: models.owned,
+    	as : "owned by",
+        where : {userId: req.user.id}
+    }] 
+     }).then(books => {
    
   
       console.log(books);
+      console.log(books[0].get());
       res.render('profile',{
       user: req.user,
       book: books
